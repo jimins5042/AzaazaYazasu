ddd6674e1cb1cebac7a7937e2ba6214a42eb7454
timetable5678 <coolcats6762@gmail.com>
2023-08-28 01:30:47 +0900

    챗봇 대화창에 이미지 링크 삽입 구현

--- src/main/java/Practice/ShoppingMall/controller/ProductController.java
+++ src/main/java/Practice/ShoppingMall/controller/ProductController.java
@@ -22,16 +22,9 @@ public class ProductController {
     private final ProductMapper productMapper;
     private final ChatService chatService;
     private final ChatgptService chatgptService;
-/*
-    @PostMapping("/api/v1/chat-gpt")
-    @ResponseBody
-    public String test(@RequestBody String question){
-        return chatService.getChatResponse(question);
-        //\n\nAs an AI language model, I don't have feelings, but I'm functioning well. Thank you for asking. How can I assist you today?
-    }
-*/
+
     @GetMapping("/main")
-    public String mainPage(Model model){
+    public String mainPage(Model model) {
         log.info("= 메인 페이지 =");
         //model.addAttribute("products", productMapper.findAll());
         model.addAttribute("products", productMapper.pagenation());
@@ -42,13 +35,12 @@ public class ProductController {
 
     @GetMapping("/chat")
     public String chat(Model model) {
-       // log.info(""+botMessages.size());
-        if(botMessages.size() == 0){
+        // log.info(""+botMessages.size());
+        if (botMessages.size() == 0) {
             botMessages.add("1번 질문");
-        }else{
-            log.info("size : " + botMessages.size() + " 채팅 : " + botMessages.get(botMessages.size()-1));
+        } else {
+            log.info("size : " + botMessages.size() + " 채팅 : " + botMessages.get(botMessages.size() - 1));
         }
-        //botMessages.add("어떤 서비스를 찾으시나요?");
         model.addAttribute("botMessages", botMessages);
 
         return "/chat";
@@ -57,32 +49,19 @@ public class ProductController {
     @PostMapping("/chat")
     @ResponseBody
     public String sendMessage(@RequestBody String userMessage) {
-        // 챗봇 엔진 로직 대신 간단히 응답 메시지를 추가합니다.
 
-        String botResponse = userMessage;
-        botMessages.add(botResponse);
-        botResponse = "답변 생성중입니다! 잠시만 기다려주세요... ";
+        botMessages.add(userMessage);
 
+        String botResponse = chatService.chatQna(botMessages.size(), botMessages);
 
-        if(botMessages.size() == 2){
-            botResponse = " 2번질문";
-        }else if(botMessages.size() == 4){
-            botResponse = " 3번질문";
-        }else if(botMessages.size() == 6){
-            botResponse = " 4번질문";
-        }else if(botMessages.size() == 8){
-
-            botResponse = chatService.getChatResponse(botMessages);
-            log.info("챗 gpt : " +botResponse );
-        }
         botMessages.add(botResponse);
         log.info("user message : " + userMessage + " size : " + botMessages.size());
-        //return botResponse;
+
         return botResponse;
     }
 
     @GetMapping("/list")
-    public String listPage(Model model){
+    public String listPage(Model model) {
         log.info("= 더보기 페이지 =");
         //model.addAttribute("products", productMapper.findAll());
         model.addAttribute("products", productMapper.findAll());
@@ -91,7 +70,7 @@ public class ProductController {
 
 
     @GetMapping("/product/{productId}")
-    public String productPage(@PathVariable(name = "productId") Integer productId, Model model){
+    public String productPage(@PathVariable(name = "productId") Integer productId, Model model) {
         log.info("= 상품 상세 페이지 =" + productId);
         Product product = productMapper.findById(productId);
         model.addAttribute("product", product);
@@ -100,31 +79,30 @@ public class ProductController {
     }
 
     @GetMapping("/product")
-    public String productPage(){
+    public String productPage() {
 
         ArrayList<Product> list = productMapper.findAll();
 
-        for(Product product : list){
+        for (Product product : list) {
             log.info(product.getProductName());
         }
         return "/main";
     }
 
     @GetMapping("/add")
-    public String addProductPage(){
+    public String addProductPage() {
         log.info("= 상품추가 페이지 =");
 
         return "/addProduct";
     }
 
     @PostMapping("/add")
-    public String addProductPage(@ModelAttribute Product product, RedirectAttributes redirectAttributes){
+    public String addProductPage(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
         log.info("= 상품추가 성공 =");
         productMapper.insertProduct(product);
         redirectAttributes.addAttribute("productId", productMapper.lastOne().getProductId());
         return "redirect:/product/{productId}";
     }
-
 
 
 }
--- src/main/java/Practice/ShoppingMall/service/ChatService.java
+++ src/main/java/Practice/ShoppingMall/service/ChatService.java
@@ -2,30 +2,78 @@ package Practice.ShoppingMall.service;
 
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
-public class ChatService{
+public class ChatService {
 
     private final ChatgptService chatgptService;
 
+    public String chatQna(Integer size, List<String> botMessages){
+        String botResponse = "답변 생성중입니다! 잠시만 기다려주세요... ";
+        if (size== 2) {
+            botResponse = " 2번질문";
+        } else if (size == 4) {
+            botResponse = " 3번질문";
+        } else if (size == 6) {
+            botResponse = " 4번질문";
+        } else if (size == 8) {
+            botResponse = getFlowerImagin(getChatResponse(botMessages));
+        }
+        return botResponse;
+    }
+
+
     public String getChatResponse(List<String> list) {
-        String question = "You are a machine that recommends flowers to customers.\n" +
+
+        String question = "Forget the conversation we've had so far.\n" +
+                "You are a machine that recommends flowers to customers.\n" +
                 "You should listen to several questions and answers from the customer and answer the name of the flower you recommend and why.\n" +
                 "The answer should be concise and friendly.\n" +
+                "Recommend flowers that are common around you.\n" +
                 "The answer should be in Korean.\n" +
                 "Here are the answers to the questions and questions provided to the customer. ";
         String prompt = "";
-        for(int i = 0; i < 7; i = i+2){
+        for (int i = 0; i < 7; i = i + 2) {
             prompt += "질문 : " + list.get(i) + " 대답 : " + list.get(i + 1);
         }
-
 
         question += prompt;
 
         // ChatGPT 에게 질문을 던집니다.
         return chatgptService.sendMessage(question);
-    }}
+
+    }
+
+    public String getFlowerImagin(String answer) {
+        String question =
+                /*
+                    앞의 대화는 잊고, 현제 제공하는 문장에서 추천하는 꽃의 이름을 말해라.
+                    이때 대답 형식은 다음과 같다.
+                    flower : 꽃 이름
+                 */
+                "Forget the conversation we've had so far.\n" +
+                "Please tell me the name of the flower you recommend in the competition below.\n" +
+                "At this time, you should only tell the name of the flower.\n" +
+                "The answer format is as follows.\n" +
+                "flower : flower name\n" +
+                "## DO NOT RESPOND TO INFO BLOCK ## " + answer;
+        String image = chatgptService.sendMessage(question);
+
+        int i = image.indexOf(":");
+        image = image.substring(i+1);
+        log.info("" + image);
+
+        image = "https://source.unsplash.com/random/400x350/?"+ image;
+        image = "<img src='" + image + "' alt='...'/> <br><br>" + answer +
+                "<br><br> 구매를 원하시면 '!구매'를, 다른 꽃 추천을 받고 싶다면 '!다시'를 입력해주세요. " ;
+        log.info(image);
+        // ChatGPT 에게 질문을 던집니다.
+        return image;
+    }
+}
