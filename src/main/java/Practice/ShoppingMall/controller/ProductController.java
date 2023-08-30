35cd4d29326188bf6ec8e3910ef99e905b8460c6
timetable5678 <coolcats6762@gmail.com>
2023-08-30 19:59:07 +0900

    css 라이브러리로 변경

--- src/main/java/Practice/ShoppingMall/controller/ProductController.java
+++ src/main/java/Practice/ShoppingMall/controller/ProductController.java
@@ -8,7 +8,6 @@ import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
